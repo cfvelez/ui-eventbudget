a7f8abb7f81056b8efe81875b0c90ce4f036d2db
Carlos Velez <cfvelez9@gmail.com>
2020-06-14 19:29:25 +0200

    UI - Version Beta - AWS Environment - Auth - Google debug in AWS

--- src/features/app/ui/login/login.tsx
+++ src/features/app/ui/login/login.tsx
@@ -78,7 +78,8 @@ export const Login: React.FunctionComponent<{}> = () => {
     const receivedToken = gtoken !== undefined && gtoken !== null ? gtoken : "";
     AuthMng.logout();
     if (receivedToken !== "") {
-      validateToken(receivedToken);
+      console.log(receivedToken);
+      //validateToken(receivedToken);
     }
   }, []);
 
